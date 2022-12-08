06f4f12b05d21aaaa2d75aea35117a85bb473e6b
marnix.ramondt@valcon.com <marnix.ramondt@valcon.com>
2022-12-08 08:42:37 +0000

    Trial Commit
    DataBricks academy

---
+++ ExampleFolder/ExampleNotebook.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %python
+# MAGIC 
+# MAGIC print("This is a test notebook created for the DataBricks academy")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC This will create a markdown codeblock
+# MAGIC 
+# MAGIC ### Title
